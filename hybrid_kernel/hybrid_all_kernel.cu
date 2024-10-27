b175f235e957c83688c7213dcf5bfd275c317ccd
ZhonggenLi <57128762+ZhonggenLi@users.noreply.github.com>
2024-10-27 16:20:55 +0800

    Update hybrid_all_kernel.cu

--- hybrid_kernel/hybrid_all_kernel.cu
+++ hybrid_kernel/hybrid_all_kernel.cu
@@ -10,6 +10,12 @@
 #include <mma.h>
 #include <cuda_runtime.h>
 
+#include <thrust/sort.h>
+#include <thrust/execution_policy.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/unique.h>
+
 #include "config.h"
 
 #define WPBMore 6
@@ -203,6 +209,203 @@ __global__ void spmm_forward_cuda_kernel_arbi_warps_hybrid_GIN_final_fused(
 // const int node_num = 410236, edge_num = 4878875, embedding_dim = 32;
 // const int node_num = 1710902, edge_num = 3636546, embedding_dim = 32;
 // const int node_num = 19717, edge_num = 88676, embedding_dim = 32;
+
+__device__ __forceinline__ void inplace_deduplication_gpu(int *array, int length,
+                                                      int *loc) {
+  int cur = 1;
+  while (cur < length) {
+    if (array[cur] != array[cur - 1]) {
+      (*loc)++;
+      array[(*loc)] = array[cur];
+    }
+    cur++;
+  }
+}
+__device__ __forceinline__ int binarysearch(int *arr, int size, int target) {
+  int left = 0;
+  int right = size - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == target) {
+      while (mid > 0 && arr[mid - 1] == target) {
+        mid--;
+      }
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+__global__ void generate_edgetocolumn(int *nodePointer, int *edgelist,
+                                      int *edgelist_sort, int *edgetocol,
+                                      int *blockpartition, int *blocknum,
+                                      int blockSize_h, int blockSize_w,
+                                      int num_nodes, int* hybrid_type) {
+  int winId = blockIdx.x; // each warp one window
+  unsigned block_start = nodePointer[winId * blockSize_h];
+  unsigned block_end =
+      nodePointer[min(winId * blockSize_h + blockSize_h, num_nodes)];
+  unsigned num_window_edges = block_end - block_start;
+  if (num_window_edges == 0)
+    return;
+  const unsigned threadPerBlock = blockDim.x * blockDim.y;
+  int *start = edgelist_sort + block_start;
+  int size = 0;
+  inplace_deduplication_gpu(start, num_window_edges, &size);
+  int num = (size + blockSize_w) / blockSize_w;
+  atomicAdd(blocknum, num);
+  blockpartition[winId] = num;
+//   hybrid_type[winId] = (size > 32 || (float)size * 0.19854024 - ((float)num_window_edges / (num * 16 * 8)) * 6.578043 - 3.14922857 > 0) ? 0:1;
+  hybrid_type[winId] = (float)size * 0.19854024 - ((float)num_window_edges / (num * 16 * 8)) * 6.578043 - 3.14922857 ? 0:1;
+//   hybrid_type[winId] = 1; 
+  for (unsigned idx = block_start; idx < block_end; idx += 1) {
+    int index = binarysearch(start, size + 1, edgelist[idx]);
+    edgetocol[idx] = index;
+	if(index / BLK_W == 536870911) printf("Find %d\n", idx);
+  }
+}
+void generate_edgetocolumn_cuda(int *nodePointer, int *edgelist,
+                                int *edgelist_sort, int *edgetocol,
+                                int *blockpartition, int *blocknum,
+                                int blockSize_h, int blockSize_w,
+                                int num_nodes, int* hybrid_type) {
+  int block_size = 1;
+  int window_count = (num_nodes + blockSize_h - 1) / blockSize_h;
+  int block_size1 = 128;
+  int block_count1 = (window_count + 127) / 128;
+  generate_edgetocolumn<<<window_count, block_size>>>(
+      nodePointer, edgelist, edgelist_sort, edgetocol, blockpartition, blocknum,
+      blockSize_h, blockSize_w, num_nodes, hybrid_type);
+
+//   cudaError_t error = cudaGetLastError();
+//   if (error != cudaSuccess) {
+//     printf("CUDA error: %s\n", cudaGetErrorString(error));
+//     exit(-1);
+//   }
+}
+__global__ void fill_segment(int *nodePointer, int *seg_out, int blockSize_h,
+                             int blockSize_w, int num_nodes) {
+  int tid = threadIdx.x;
+  int winId = blockIdx.x; 
+  unsigned block_start = nodePointer[winId * blockSize_h];
+  unsigned block_end =
+      nodePointer[min(winId * blockSize_h + blockSize_h, num_nodes)];
+  unsigned num_window_edges = block_end - block_start;
+  const unsigned threadPerBlock = blockDim.x * blockDim.y;
+  for (unsigned idx = tid; idx < num_window_edges; idx += threadPerBlock) {
+    seg_out[block_start + idx] = winId;
+  }
+}
+void fill_segment_cuda(int *nodePointer, int *seg_out, int blockSize_h,
+                       int blockSize_w, int num_nodes) {
+  int block_size = 512;
+  int window_count = (num_nodes + blockSize_h - 1) / blockSize_h;
+  fill_segment<<<window_count, block_size>>>(nodePointer, seg_out, blockSize_h,
+                                             blockSize_w, num_nodes);
+//   cudaError_t error = cudaGetLastError();
+//   if (error != cudaSuccess) {
+//     printf("CUDA error: %s\n", cudaGetErrorString(error));
+//     exit(-1);
+//   }
+}
+__global__ void fill_edgeToRow(int *edgeToRow, int *nodePointer,
+                               int num_nodes) {
+  int tid = blockDim.x * blockIdx.x + threadIdx.x;
+  int nid = tid / 32;
+  int laneid = tid % 32;
+  if (nid < num_nodes) {
+#pragma unroll
+    for (int eid = nodePointer[nid] + laneid; eid < nodePointer[nid + 1];
+         eid += 32) {
+      edgeToRow[eid] = nid;
+    }
+  }
+}
+void fill_edgeToRow_cuda(int *edgeToRow, int *nodePointer, int num_nodes) {
+  int wrap_size = 32;
+  int block_size = 1024;
+  int grid_size = (num_nodes * wrap_size + block_size - 1) / block_size;
+  fill_edgeToRow<<<grid_size, block_size>>>(edgeToRow, nodePointer, num_nodes);
+//   cudaError_t error = cudaGetLastError();
+//   if (error != cudaSuccess) {
+//     printf("CUDA error: %s\n", cudaGetErrorString(error));
+//     exit(-1);
+//   }
+}
+
+std::vector<torch::Tensor> preprocess(torch::Tensor edgeList_tensor, 
+                torch::Tensor nodePointer_tensor,
+                int num_nodes, 
+                int edge_num,
+                int rw_num
+                ){
+
+    torch::Tensor blockPartition_tensor; 
+    torch::Tensor edgeToColumn_tensor;
+    torch::Tensor edgeToRow_tensor;
+    torch::Tensor hybrid_type_tensor;
+
+    auto d_edgeList = edgeList_tensor.data<int>();
+    auto d_nodePointer = nodePointer_tensor.data<int>();
+
+    int* d_blockPartition, * d_edgeToColumn, * d_edgeToRow, * d_hybrid_type;
+
+    cudaMalloc((void**)&d_blockPartition, sizeof(int) * rw_num);
+    cudaMemset(d_blockPartition, 0, rw_num);
+
+    cudaMalloc((void**)&d_edgeToColumn, sizeof(int) * edge_num);
+    cudaMemset(d_edgeToColumn, 0, edge_num);
+
+    cudaMalloc((void**)&d_edgeToRow, sizeof(int) * edge_num);
+    cudaMemset(d_edgeToRow, 0, edge_num);
+
+    cudaMalloc((void**)&d_hybrid_type, sizeof(int) * rw_num);
+    cudaMemset(d_hybrid_type, 0, rw_num);
+
+    auto opts = torch::TensorOptions().dtype(torch::kInt).device(torch::kCUDA);
+    blockPartition_tensor = torch::from_blob(d_blockPartition, {rw_num}, opts);
+    edgeToColumn_tensor = torch::from_blob(d_edgeToColumn, {edge_num}, opts);
+    edgeToRow_tensor = torch::from_blob(d_edgeToRow, {edge_num}, opts);
+    hybrid_type_tensor = torch::from_blob(d_hybrid_type, {rw_num}, opts);
+
+    int *seg_out, *block_num;
+
+    cudaMalloc((void**)&seg_out, sizeof(int) * edge_num);
+    cudaMemset(seg_out, 0, edge_num);
+
+    cudaMalloc((void**)&block_num, sizeof(int) * 1);
+    cudaMemset(block_num, 0, 1);
+
+    fill_edgeToRow_cuda(d_edgeToRow, d_nodePointer, num_nodes);
+    int block_counter = 0;
+    fill_segment_cuda(d_nodePointer, seg_out, BLK_H, BLK_W, num_nodes);
+
+    thrust::device_ptr<int> Seg = thrust::device_pointer_cast(seg_out);
+
+    thrust::device_vector<int> deviceSeg(Seg, Seg + edge_num);
+
+    thrust::device_ptr<int> EL = thrust::device_pointer_cast(d_edgeList);
+
+    thrust::device_vector<int> deviceEL(EL, EL + edge_num);
+
+    auto begin = thrust::make_zip_iterator(
+        thrust::make_tuple(deviceSeg.begin(), deviceEL.begin()));
+    auto end = thrust::make_zip_iterator(
+        thrust::make_tuple(deviceSeg.end(), deviceEL.end()));
+
+    thrust::sort(thrust::device, begin, end);
+
+    generate_edgetocolumn_cuda(
+        d_nodePointer, d_edgeList, thrust::raw_pointer_cast(&deviceEL[0]), d_edgeToColumn,
+        d_blockPartition, block_num, BLK_H, BLK_W, num_nodes, d_hybrid_type);
+
+    torch::Tensor row_nzr = torch::tensor({0}, opts), col_nzr = torch::tensor({0}, opts);
+
+    return {blockPartition_tensor, edgeToColumn_tensor, edgeToRow_tensor, hybrid_type_tensor, row_nzr, col_nzr};
+}
 
 std::vector<torch::Tensor> spmm_forward_plus(
         torch::Tensor input,
